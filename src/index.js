d87bfb13fe1f3b87187ba1fa21006f730f29cec4
eglyakov <eglyakov@gmail.com>
2021-03-03 00:48:19 +0300

    feat: add min max avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+
+  let res = array[0];
+
+  for(let i = 0; i < array.length; i++) {
+      if(array[i] < res) {
+          res = array[i];
+      }
+  }
+
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+
+  let res = array[0];
+
+  for(let i = 0; i < array.length; i++) {
+      if(array[i] > res) {
+          res = array[i];
+      }
+  }
+
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0) return 0;
+
+    let res = 0,
+        avg = 0;
+
+    for(let i = 0; i < array.length; i++) {       
+        res += array[i];    
+    }
+
+    avg = res / array.length;
+    return avg;
 }
